2710d424c904a6d6c15f4b46bc34d11658f3d5dc
SamirGrini <sgrini.dev@gmail.com>
2025-01-30 09:16:45 +0100

    Salutation Aventurier

---
+++ exercices.js
@@ -0,0 +1,8 @@
+function salutations(nomSorcier) {
+  console.log(
+    "Salutations Aventurier ! Je me nomme ${nom_sorcier} pour vous servir."
+  );
+}
+
+const nom_sorcier = "Archibad";
+salutations(nom_sorcier);
