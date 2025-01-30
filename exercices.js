e81babae9811656a5c299e9b5f5f86987d9b2be4
SamirGrini <sgrini.dev@gmail.com>
2025-01-30 09:42:04 +0100

    Tarif d'une potion

--- exercices.js
+++ exercices.js
@@ -1,8 +1,17 @@
-function salutations(nomSorcier) {
+/* function salutations(nomSorcier) {
   console.log(
-    "Salutations Aventurier ! Je me nomme ${nom_sorcier} pour vous servir."
+    `Salutations Aventurier ! Je me nomme ${nom_sorcier} pour vous servir.`
   );
 }
 
 const nom_sorcier = "Archibad";
-salutations(nom_sorcier);
+salutations(nom_sorcier); */
+
+/*function prix_potion(prix_potion, quantite) {
+  return prix_potion * quantite;
+}
+
+let prixTotal = prix_potion(3, 10);
+console.log(
+  `Le prix total pour 3 potions est de ${prixTotal} rubis cher Aventurier. `
+); */
